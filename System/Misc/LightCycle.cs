1d24be61e4839f3bf8e0a88b740ebee80f245907
slawdis <35615205+slawdis@users.noreply.github.com>
2020-09-08 22:34:48 -0700

    Update LightCycle.cs

--- System/Misc/LightCycle.cs
+++ System/Misc/LightCycle.cs
@@ -8,9 +8,9 @@ namespace Server
 	public class LightCycle
 	{
 		public const int DayLevel = 0;
-		public const int NightLevel = 26;
-		public const int DungeonLevel = 30;
-		public const int CaveLevel = 22;
+		public const int NightLevel = 20;
+		public const int DungeonLevel = 26;
+		public const int CaveLevel = 20;
 		public const int JailLevel = 13;
 
 		private static int m_LevelOverride = int.MinValue;
